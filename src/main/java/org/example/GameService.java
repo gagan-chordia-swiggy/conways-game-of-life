3c4097b83e7a3fdc5ef1ed87511ea0b7631c5dcb
gagan-chordia-swiggy <gagan.chordia_ftc@external.swiggy.in>
2024-01-27 14:01:55 +0530

    32. Refactored Main class to GameService class

--- src/main/java/org/example/GameService.java
+++ src/main/java/org/example/GameService.java
@@ -4,7 +4,7 @@ package org.example;
  * Conway's Game of Life
  * Reference: <a href="https://pi.math.cornell.edu/~lipa/mec/lesson6.html#:~:text=For%20each%20generation%20of%20the,it%20has%203%20live%20neighbors">Rules of the game by Cornell University</a>
  */
-public class Main {
+public class GameService {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
